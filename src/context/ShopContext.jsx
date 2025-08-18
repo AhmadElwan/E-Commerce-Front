6c61b76bdd945f1bfba82e94d45c848bab6ae30e
AhmadElwan1 <Ahmad.Elwan@optimumpartners.co>
2025-08-18 18:58:07 +0300

    Added Hero section and Latest Collection section

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+import { products } from "../assets/frontend_assets/assets";
+
+const ShopContext = createContext();
+
+export const ShopContextProvider = (props) => {
+
+    const currency = 'JD';
+    const delivery_fee = 2;
+
+    const value = {
+        products, currency, delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export const useShopContext = () => {
+    return useContext(ShopContext);
+}
